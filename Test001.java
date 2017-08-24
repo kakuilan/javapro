de74b49df405fd45f23ac88f31322c489c20fd6d
kakuilan <kakuilan@163.com>
2017-08-24 17:43:34 +0800

    变量声明
    变量声明

---
+++ Test001.java
@@ -0,0 +1,27 @@
+public class Test001 {
+	public static void main(String[] args) {
+		//变量声明
+		byte a = 56;
+		//长整型
+		long bigVal = 98765432123456789L;
+		//以0开头的八进制整数
+		int octalVal = 0123;
+		//以0x开头的十六进制整数
+		int hexVal1 = 0x13;
+		int hexVal2 = 0Xaf;
+		//二进制整数
+		int binVal1 = 0b11010100;
+		int binVal2 = 0B01101001;
+
+		System.out.println(a);
+		System.out.println(bigVal);
+		System.out.println(octalVal);
+		System.out.println(hexVal1);
+		System.out.println(hexVal2);
+		System.out.println(binVal1);
+		System.out.println(binVal2);
+
+
+
+	}
+}
