af01cb0c1f9884a056ce3aeae964a15d34929622
kakuilan <kakuilan@163.com>
2017-08-24 16:52:31 +0800

    javadoc
    javadoc

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
+
 public class HelloWorld {
-	//JAVA程序的入口方法，程序将从着拉开始执行
+
+
+	/**
+	 * JAVA程序的入口方法,程序将从着拉开始执行
+	 * @author kakuilan
+	 * @version 0.0.1
+	 * @param args 参数
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
