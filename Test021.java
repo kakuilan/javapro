c9590f080d57cdcbf6a937bfc6f3a0e76da50f4e
kakuilan <kakuilan@163.com>
2017-08-25 17:59:05 +0800

    while循环

---
+++ Test021.java
@@ -0,0 +1,12 @@
+//while循环
+public class Test021 {
+
+	public static void main(String[] args) {
+		int count = 0;
+		while (count < 10) {
+			System.out.println(count);
+			count++;
+		}
+		System.out.println("循环结束!");
+	}
+}
