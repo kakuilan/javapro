5e18316a3ae0d5a5823c42849dd7e2b72debbc6b
kakuilan <kakuilan@163.com>
2017-08-25 10:56:23 +0800

    浮点数类型

---
+++ Test003.java
@@ -0,0 +1,14 @@
+//浮点数值
+public class Test003  {
+
+	public static void main(String[] args) {
+		//定义一个32位的二进制数,最高位是符号位
+		int binVal = 0B10000000000000000000000000000011;
+		double pi = 3.1415926536;
+		System.out.println(binVal);
+		System.out.println(pi);
+		double height = 8848.23;
+		System.out.println(height);
+	}
+
+}
