c784a4346d5bf913a0276f1e7ced9c69d87342bd
kakuilan <kakuilan@163.com>
2017-08-25 11:03:54 +0800

    布尔类型

---
+++ Test004.java
@@ -0,0 +1,14 @@
+//布尔型
+public class Test004 {
+
+	public static void main(String[] args) {
+		//定义b1值为true
+		boolean b1 = true;
+		//定义b2值为false
+		boolean b2 = false;
+		//使用boolean类型的值和字符串进行连接运算,boolean类型的值会自动转换为字符串
+		String str = true + "";
+		System.out.println(str);
+	}
+
+}
