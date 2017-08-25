c4f77f20cda9be89e4af67c8dd7ad9a9fb3a6506
kakuilan <kakuilan@163.com>
2017-08-25 15:08:21 +0800

    直接量的赋值

---
+++ Test011.java
@@ -0,0 +1,21 @@
+//直接量的赋值
+public class Test011 {
+	public static void main(String[] args) {
+		int a = 5;
+		char c = 'a';
+		boolean b = true;
+		float f = 5.12f;
+		double d = 4.12;
+		String author = "lkk";
+		String book = "Crazy java";
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(author);
+		System.out.println(book);
+	}
+
+}
