819386cf90b75a6d8334f9ad2b1919c8b60af310
kakuilan <kakuilan@163.com>
2017-08-28 14:25:18 +0800

    数组foreach遍历

---
+++ Test025.java
@@ -0,0 +1,12 @@
+public class Test025{
+	public static void main(String[] args) {
+		String[] books = {"轻量级java",
+			"架构与未来",
+			"hello world"};
+		//使用foreach遍历
+		for (String book : books) {
+			System.out.println(book);
+		}
+
+	}
+}
