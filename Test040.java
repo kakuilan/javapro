ed2d96f0d58f1ff2b362bfa8b31ac1ad403c9e70
kakuilan <kakuilan@163.com>
2017-08-29 18:17:48 +0800

    方法重载

--- Test040.java
+++ Test040.java
@@ -1,4 +1,6 @@
 //方法重载
+import java.util.Arrays;
+
 public class Test040 {
 	//下面定义2个test方法，但方法的形参列表不同
 	//系统可以区分这2种方法,这被称为方法重载
@@ -10,11 +12,18 @@ public class Test040 {
 	public void test(String msg) {
 		System.out.println("重载的test方法" + msg);
 	}
-	
+
+	public void test(String... books) {
+		System.out.println("形参长度可变的test方法");
+		System.out.println(Arrays.deepToString(books));
+	}
+
 	public static void main(String[] args) {
 		Test040 obj = new Test040();
 		obj.test();
 		obj.test("Hello World.");
+		obj.test("初学java","java入门");
+		obj.test(new String[]{"Hello","Java"});
 	}
 
 }
