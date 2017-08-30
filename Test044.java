f340bf43d42ff3305ba7dbaf760bd425ca0c7f63
kakuilan <kakuilan@163.com>
2017-08-30 16:56:57 +0800

    package包的使用

---
+++ Test044.java
@@ -0,0 +1,9 @@
+//package包的使用
+//javac -d . Test044.java
+//上面会在当前目录下新建lee目录保存编译的Test044.class
+package lee;
+public class Test044 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
