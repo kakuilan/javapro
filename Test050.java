0a6f849df8092c40e998a348a539932fa7c1268d
kakuilan <kakuilan@163.com>
2017-09-06 12:50:51 +0800

    重写父类的方法

--- Test050.java
+++ Test050.java
@@ -3,12 +3,23 @@ class Bird{
 	public void fly(){
 		System.out.println("我在天空里自由飞翔...");
 	}
+
+	//test方法是private方法权限，子类不可访问该方法
+	private void test(){
+		
+	}
+
 }
 
 public class Test050 extends Bird{
 	//重写Bird类的fly方法
 	public void fly(){
 		System.out.println("我只能在地上奔跑...");
+	}
+
+	//此处并不是方法重写，所以可以增加static关键字
+	public static void test(){
+	
 	}
 
 
