c222746c71ff8c52a060f51082ab3545703c13d9
kakuilan <kakuilan@163.com>
2017-09-20 13:42:46 +0800

    一个接口继承多个接口

---
+++ Test075.java
@@ -0,0 +1,23 @@
+//一个接口继承多个接口
+interface interfaceA {
+	int PROP_A = 5;
+	void testA();
+}
+
+interface interfaceB {
+	int PROP_B = 6;
+	void testB();
+}
+
+interface interfaceC extends interfaceA,interfaceB {
+	int PROP_C = 7;
+	void testC();
+}
+
+public class Test075 {
+	public static void main(String[] args) {
+		System.out.println(interfaceC.PROP_A);
+		System.out.println(interfaceC.PROP_B);
+		System.out.println(interfaceC.PROP_C);
+	}
+}
